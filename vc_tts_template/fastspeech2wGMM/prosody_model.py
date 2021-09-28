5c636e522b1a21af8f4d3c3f3f7f77464cdcd143
yuto_nishimura <yutonishimurav20512@gmail.com>
2021-09-28 13:14:40 +0000

    refactoring

--- vc_tts_template/fastspeech2wGMM/prosody_model.py
+++ vc_tts_template/fastspeech2wGMM/prosody_model.py
@@ -10,7 +10,7 @@ from vc_tts_template.utils import pad
 
 
 def encoder_init(m):
-    if isinstance(m, nn.Conv1d):
+    if isinstance(m, nn.Conv2d):
         nn.init.xavier_uniform_(m.weight, nn.init.calculate_gain("relu"))
 
 
@@ -41,34 +41,40 @@ class ProsodyExtractor(nn.Module):
 
     def forward(self, mels, durations):
         # expected(B, T, d_mel)
-        durations = durations.detach().cpu().numpy()  # 念のため, 伝播を防ぐ.
-        # melをphone単位のsegmentに分割
+        durations = durations.detach().cpu().numpy()  # detach
         output_sorted, src_lens_sorted, segment_nums, inv_sort_idx = self.mel2phone(mels, durations)
         outs = list()
         for output, src_lens in zip(output_sorted, src_lens_sorted):
+            # output: (B, T_n, d_mel). T_n: length of phoneme
             # convolution
             mel_hidden = self.convnorms(output.unsqueeze(1))
             # Bi-GRU
             mel_hidden = pack_padded_sequence(mel_hidden.squeeze(1), src_lens, batch_first=True)
             out, _ = self.bi_gru(mel_hidden)
+            # out: (B, T_n, d_out)
             out, _ = pad_packed_sequence(out, batch_first=True)
-            outs.append(out[:, -1, :])  # 最終時刻の出力を利用
+            outs.append(out[:, -1, :])  # use last time
         outs = torch.cat(outs, 0)
-        out = self.phone2emb(outs[inv_sort_idx], segment_nums)
+        out = self.phone2utter(outs[inv_sort_idx], segment_nums)
         return out
 
     def mel2phone(self, mels, durations):
+        """
+        melをphone単位のsegmentに分割
+        長い順に降順ソートして, batch_sizeごとにまとめている
+        降順sortはpack_padded_sequenceの要請. batch_sizeごとにまとめたのはpadの量を削減するため
+        """
         output = list()
-        output_sorted = list()
         src_lens = list()
-        src_lens_sorted = list()
         segment_nums = list()
+        # devide mel into phone segments
         for mel, duration in zip(mels, durations):
             s_idx = 0
             cnt = 0
             for d in duration:
                 if d == 0:
-                    continue
+                    # d = 0 is only allowed for pad
+                    break
                 mel_seg = mel[s_idx: s_idx+d]
                 s_idx += d
                 cnt += 1
@@ -77,14 +83,15 @@ class ProsodyExtractor(nn.Module):
             segment_nums.append(cnt)
 
         sort_idx = np.argsort(-np.array(src_lens))
-        # 同じ値があっても元の場所に戻るのは確認済
         inv_sort_idx = np.argsort(sort_idx)
-        # padを少なくするために, batch数ごとのまとまりでpadしていく
-        # 以下のやり方は, BNをする際にbatch_size=1とならないようにするための方法
+        # Regroup phoneme segments into batch sizes.
         batch_size = mels.size(0)
+        output_sorted = list()
+        src_lens_sorted = list()
         for i in range(len(output) // batch_size):
             sort_seg = sort_idx[i*batch_size:(i+1)*batch_size]
             if i == ((len(output) // batch_size) - 1):
+                # This is a way to avoid setting batch_size=1 when doing BN.
                 sort_seg = sort_idx[i*batch_size:]
             output_seg = [output[idx] for idx in sort_seg]
             src_lens_seg = [src_lens[idx] for idx in sort_seg]
@@ -93,7 +100,10 @@ class ProsodyExtractor(nn.Module):
             src_lens_sorted.append(src_lens_seg)
         return output_sorted, src_lens_sorted, segment_nums, inv_sort_idx
 
-    def phone2emb(self, out, segment_nums):
+    def phone2utter(self, out, segment_nums):
+        """
+        音素ごとのmel segmentを, utteranceごとにまとめ直す
+        """
         output = list()
         s_idx = 0
         for seg_num in segment_nums:
@@ -122,10 +132,24 @@ class ConvNorms(nn.Module):
 
         self.convolutions = []
 
-        self.convolutions.append(
-            nn.Sequential(
+        self.convolutions += [
+            nn.Conv2d(
+                in_channels,
+                out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                bias=bias
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        ]
+
+        for _ in range(n_layers - 1):
+            self.convolutions += [
                 nn.Conv2d(
-                    in_channels,
+                    out_channels,
                     out_channels,
                     kernel_size=kernel_size,
                     stride=stride,
@@ -135,25 +159,7 @@ class ConvNorms(nn.Module):
                 ),
                 nn.BatchNorm2d(out_channels),
                 nn.ReLU()
-            )
-        )
-
-        for _ in range(1, n_layers - 1):
-            self.convolutions.append(
-                nn.Sequential(
-                    nn.Conv2d(
-                        out_channels,
-                        out_channels,
-                        kernel_size=kernel_size,
-                        stride=stride,
-                        padding=padding,
-                        dilation=dilation,
-                        bias=bias
-                    ),
-                    nn.BatchNorm2d(out_channels),
-                    nn.ReLU()
-                )
-            )
+            ]
         self.convolutions = nn.Sequential(*self.convolutions)
 
     def forward(self, x):
@@ -174,13 +180,13 @@ if __name__ == "__main__":
             [11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
             [21, 22, 23, 24, 25, 26, 27, 28, 29, 30],
         ]
-    ).unsqueeze(-1)  # (B, T, mel_bin)
+    ).unsqueeze(-1).to(device)  # (B, T, mel_bin)
     durations = torch.Tensor(
         [
             [2, 2, 2, 2, 2],
             [2, 3, 2, 0, 0],
             [10, 0, 0, 0, 0]
         ]
-    ).long()
-    out = extractor(mels.to(device), durations.to(device))
+    ).long().to(device)
+    out = extractor(mels, durations)
     print(out.size())
